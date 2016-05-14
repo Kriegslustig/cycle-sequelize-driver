40e5eee0424ef936e52f0aaa19ac8e24f6920b7f
Kriegslustig <kriegslustig@kriegslustig.me>
2016-05-14 20:31:07 +0200

    Implement a better testing system

--- test/sqlite/index.js
+++ test/sqlite/index.js
@@ -1,21 +1,22 @@
-import test from 'ava'
-import Sequelize from 'sequelize'
-import fs from 'fs'
-import Rx from 'rx'
+const test = require('tape')
+const Sequelize = require('sequelize')
+const fs = require('fs')
+const Rx = require('rx')
 
-import { makeSequelizeDriver, define, create } from '../../dist/index.js'
+const s = require(`${__dirname}/../../dist/index.js`)
+const makeSequelizeDriver = s.makeSequelizeDriver
+const define = s.define
+const create = s.create
 
 const storage = `${__dirname}/test.sqlite`
 global.sequelize = new Sequelize('test', null, null, { dialect: 'sqlite', storage })
 
-test.cb.after((t) => {
-  fs.unlink(storage, (err, data) => {
-    if (err) console.error(err)
-    t.end()
-  })
+test.onFinish((t) => {
+  fs.unlinkSync(storage)
 })
 
-test.cb('Definition operations', (t) => {
+test('Definition operations', (t) => {
+  t.plan(1)
   const input$ = Rx.Observable.of(define(
     'testset',
     { a: { type: Sequelize.STRING } },
@@ -27,54 +28,49 @@ test.cb('Definition operations', (t) => {
         if (
           s.has('testset')
         ) {
-          Rx.Observable.of(s.get('testset').count())
+          Rx.Observable.fromPromise(s.get('testset').count())
             .subscribe(
-              (n) => { if (n === 0) t.pass(); t.end() },
-              () => { t.fail('Table not created'); t.end() }
+              (n) => { t.equal(n, 0) },
+              () => { t.fail('Table not created') }
             )
         } else {
           t.fail('Definition not properly made.')
-          t.end()
         }
       },
-      (err) => { t.fail(err.message); t.end() }
+      (err) => { t.fail(err.message) }
     )
 })
 
-test.cb('Insertion operations', (t) => {
-  try {
-    const subject$ = new Rx.Subject()
-    const output$ = makeSequelizeDriver(global.sequelize)(subject$)
-    const actions$ = Rx.Observable.merge(
-      output$
-        .filter((s) => s.has('testset'))
-        .map(((executed) => (s) => {
-          if (executed) return s
-          executed = true
+test('Insertion operations', (t) => {
+  t.plan(1)
+  const subject$ = new Rx.Subject()
+  const output$ = makeSequelizeDriver(global.sequelize)(subject$)
+  const actions$ = Rx.Observable.merge(
+    output$
+      .filter((s) => s.has('testset'))
+      .map(((executed) => (s) => {
+        if (executed) return s
+        executed = true
 
-          // Create an entity
-          return create({ a: 'test1' })
-        })(false))
-        .tap((i) => (s) => {
-          if (++i > 3) { t.fail('Stopped by guard'); return t.end() }
-          s.get('testset').findOne({ where: { a: 'test1' } })
-            .then(() => { t.pass(); t.end() })
-            .catch(() => { t.fail('Failed to query DB'); t.end() })
-        })
-    )
+        // Create an entity
+        return create({ a: 'test1' })
+      })(false))
+      .tap((i) => (s) => {
+        if (++i > 3) { t.fail('Stopped by guard'); return t.end() }
+        s.get('testset').findOne({ where: { a: 'test1' } })
+          .then(() => { t.pass(); t.end() })
+          .catch(() => { t.fail('Failed to query DB'); t.end() })
+      })
+  )
 
-    subject$.onNext(
-      'testset',
-      { a: { type: Sequelize.STRING } },
-      { freezeTableName: true }
-    )
-    actions$.subscribe(
-      (op) => { subject$.onNext(op) },
-      () => { t.fail(); t.end() }
-    )
-  } catch (e) {
-    t.fail(e.message)
-    t.end()
-  }
+  subject$.onNext(
+    'testset',
+    { a: { type: Sequelize.STRING } },
+    { freezeTableName: true }
+  )
+  actions$.subscribe(
+    (op) => { subject$.onNext(op) },
+    () => { t.fail(); t.end() }
+  )
 })
 
